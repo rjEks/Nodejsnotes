58042b35ab16ece5ea98a77497264962d13f8d91
rjeks <rjekstein@gmail.com>
2021-01-11 20:57:21 -0300

    first commit

---
+++ distributed-node/recipe-api/producer-http-basic.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const server = require('fastify')();
+//Setando variaveis de ambiente
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 4000;
+
+console.log('worker pid =${process.pid}');
+
+server.get('/recipes/:id', async(req, reply) => {
+    console.log(`worker request pid=${process.pid}`);
+    const id = Number(req.params.id);
+    if(id!=42){
+       reply.statusCode=404;
+       return { error:'not_foun'};
+       }
+       return{
+           producer_pid:process.pid,           
+           recipe: {
+            id, name: "Chicken Tikka Masala",
+            steps: "Throw it in a pot...",
+            ingredients: [
+                { id: 1, name: "Chicken", quantity: "1 lb", },
+                { id: 2, name: "Sauce", quantity: "2 cups", }
+                        ]   
+                    }
+            };
+});
+server.listen(PORT, HOST, () => {
+    console.log(`Producer running at http://${HOST}:${PORT}`);
+});
